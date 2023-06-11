77e465207803f38810643b5b4fff069a05cc8f87
sanjay-ssb <129607765+sanjay-ssb@users.noreply.github.com>
2023-06-11 18:21:58 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/inventoryjava/InventoryJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventoryjava;
+
+/**
+ *
+ * @author jay01
+ */
+public class InventoryJava {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        
+    }
+}
